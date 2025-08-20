1f5b0b874c08beff97431b5eedfe6787a802b455
Sriram Pavurala <srirampavu11@gmail.com>
2025-08-20 11:11:42 +0200

    Add files via upload

---
+++ Models/GridItem.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LoginPageDemo.Models
+{
+    public class GridItem
+    {
+        [MaxLength(100)]
+        [Required]
+        public int Id { get; set; }
+        [MaxLength(100)]
+        [Required]
+        public string Name { get; set; } = null!;
+        [MaxLength(100)]
+        [Required]
+        public string City { get; set; } = null!;
+        [MaxLength(100)]
+        [Required]
+        public int Age { get; set; }
+        [MaxLength(100)]
+        [Required]
+        public string Status { get; set; } = null!;
+        [MaxLength(100)]
+        [Required]
+        public DateTime CreatedOn { get; set; }
+    }
+}
